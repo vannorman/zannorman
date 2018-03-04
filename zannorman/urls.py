a1660083f9ef8100b7f0bd7b8aea3df2ce57cd68
Charles Van Norman <ccvannorman@gmail.com>
2018-03-04 10:06:03 -0800

    address

--- zannorman/urls.py
+++ zannorman/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
 
 	url(r'^$', zannorman.views.home),
 	url(r'^blog/$', zannorman.views.blog_base), # Teacher final
-
+	url(r'^address/$', zannorman.views.simple_page('address.html')), # Teacher final
 	url(r'^blog/(.*)/$', zannorman.views.blog), # Teacher final
 	url(r'^.well-known/acme-challenge/p_LTkY9QHhcECb6Lv1UZWYQ6rawjuQLnUAdBdZZE9kk', zannorman.views.file_a),
 	url(r'^.well-known/acme-challenge/v9b5S4UbuLtvh_PwuhqjfOUnVfiulJSmFCYkNHtD6mA', zannorman.views.file_b),
