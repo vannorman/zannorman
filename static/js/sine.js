319cfb195e12a592fc71df1f668b12f610021b74
Charlie Zannorman <charlie@zannorman.com>
2018-07-08 18:15:41 -1000

    wavy waves wave

--- static/js/sine.js
+++ static/js/sine.js
@@ -1,143 +1,93 @@
 var d1 = 1.1;
-var d2 = 0.5;
-var speed = 0.2;
+var d2 = 10.5;
+var speed = 1;
 var frequency = 20;
-
-function Lerp(fr,to,dt){
-	var d = to - fr;
-	fr += d * dt * dt;
-	return fr;
-}
-
+var step = -4;
+var mode = 3;
 
 $(document).ready(function(){
 	init();
-});
-       function showAxes(ctx,axes) {
-            var width = ctx.canvas.width;
-            var height = ctx.canvas.height;
-            var xMin = 0;
-            
-            ctx.beginPath();
-            ctx.strokeStyle = "rgba(100,30,0,0.4)";
-            
-            // X-Axis
-            ctx.moveTo(xMin, height/2);
-            ctx.lineTo(width, height/2);
-            
-            // Y-Axis
-            ctx.moveTo(width/2, 0);
-            ctx.lineTo(width/2, height);
-            // Starting line
-            ctx.moveTo(0, 0);
-            ctx.lineTo(0, height);
-            
-            ctx.stroke();
-        }
-        function drawPoint(ctx, y) {            
-            var radius = 3;
-            ctx.beginPath();
-            // Hold x constant at 4 so the point only moves up and down.
-            ctx.arc(4, y, radius, 0, 2 * Math.PI, false);
-            ctx.fillStyle = 'red';
-            ctx.fill();
-            ctx.lineWidth = 1;
-            ctx.stroke();
-        }
-        function plotSine(ctx, xOffset, yOffset) {
-            var width = ctx.canvas.width;
-            var height = ctx.canvas.height;
-            var scale = 20;
-            ctx.beginPath();
-            ctx.lineWidth = 2;
-            ctx.strokeStyle = "rgba(66,44,255,0.2)";
-            // console.log("Drawing point...");
-            // drawPoint(ctx, yOffset+step);
-            
-            var x = 4;
-            var y = 0;
-            var amplitude = 40;
-            //ctx.moveTo(x, y);
-            ctx.moveTo(x, 50);
-			
-            while (x < width) {
-				// width is 500
-				// x is zero to width
-//                y = height/2 + amplitude * Math.sin(Math.sin(Math.pow(
-//					(x+xOffset)/frequency,dt))+dt1);
-				var dx = (x+xOffset)/frequency ;
-                y = height/2 + amplitude * Math.sin(Math.pow(d1,dx - d2));
-/*
-				if (dn < 0.1){
-					if (n1 > n2) {
-						frequency -= dn;
-//						frequency = Math.pow(frequency,1.001);
-					} else {
-						frequency += dn;
-						//frequency = Math.pow(frequency,0.999);
+	$('body').append("<div id='debug'></div>");
+	$(document).mousemove(function( event ) {
+//		frequency = event.pageX;
+//		step = event.pageY;
+	});
 
-					}
-				} else {
-					if (frequency > origFrequency) {
-						frequency -= frst;
-					} else {
-						frequency += frst;
+
+});
+
+function plotSine(ctx, xOffset, yOffset) {
+	$('#debug').text('step:'+step.toFixed(2)+', freq:'+frequency+", d1:"+d1.toFixed(2)+", d2:"+d2.toFixed(2));
+	var width = ctx.canvas.width;
+	var height = ctx.canvas.height;
+	var scale = 20;
+	ctx.beginPath();
+	ctx.lineWidth = 2;
+	ctx.strokeStyle = "rgba(255,125,0,0.2)";
 	
-					}
-//						frequency = origFrequency; //Lerp(frequency,origFrequency,0.1);
-				}
-*/
-				// if focus is 0, n is 500
-				// if focus is 1, n is 250
-				// if focus is 2, n is 250
-//				if (x < width * (focus + 1)/4)  frequency = Math.pow(frequency,0.999);
-//				else if (x < width * (focus + 2)/4) frequency = frequency;
-//				else  frequency = Math.pow(frequency,1.001);
-					
-                ctx.lineTo(x, y);
-				ctx.lineTo(x,ctx.canvas.height);
-				x++;
-//				step1 += stepm;
-//                x+= 1 * step1;
-//                console.log("x="+x+" y="+y);
-            }
-            ctx.stroke();
-            ctx.save();
-//            console.log("Drawing point at y=" + y);
-            drawPoint(ctx, y);
-            ctx.stroke();
-            ctx.restore();
-        }
-        function draw() {
-			frequency = 20;
-            var canvas = document.getElementById("canvas");
-            var context = canvas.getContext("2d");
-            context.clearRect(0, 0, 500, 500);
-            showAxes(context);
-            context.save();            
-            
-            plotSine(context, step, 50);
-            context.restore();
-			step += speed;
-			// step1 += step1m; 
-//            step += speed + step1;
-            window.requestAnimationFrame(draw);
-        }
-//        function spirograph() {            
-//            var canvas2 = document.getElementById("canvas2");
-//            var context = canvas2.getContext("2d");
-//            
-//            showAxes(context);
-//            context.save();
-//            // var imageData = context.getImageData(0, 0, canvas.width, canvas.height);
-//            var step = 4;
-//            for (var i = -4; i < canvas.height; i += step) {
-//                // context.putImageData(imageData, 0, 0);
-//                plotSine(context, i, 54 + i);
-//            }
-//        }
-        function init() {
-            window.requestAnimationFrame(draw);
-            //spirograph();
-        }
-        var step = -4;
+	var x = 4;
+	var y = 0;
+	var amplitude = 40;
+	ctx.moveTo(x, 50);
+	yy = "";
+	while (x < width) {
+		var dx = (x+xOffset)/frequency ;
+		y = height/2 + amplitude * Math.sin(dx);
+		y2 = 100 + (-x * 25 + ctx.canvas.width)    ;
+		if (y < y2){
+			y = y2;
+		}
+		if (x < 10) {
+			ctx.moveTo(x,y);
+		}
+		ctx.lineTo(x, y);
+		ctx.lineTo(x,ctx.canvas.height);
+			
+		x++;
+	}
+	ctx.stroke();
+}
+function draw() {
+	var canvas = document.getElementById("canvas");
+	var context = canvas.getContext("2d");
+	context.clearRect(0, 0, 500, 500);
+	plotSine(context, step, 50);
+	step -= speed;
+	
+	window.requestAnimationFrame(draw);
+}
+function init() {
+	window.requestAnimationFrame(draw);
+}
+
+
+document.onkeydown = checkKey;
+function checkKey(e) {
+    e = e || window.event;
+
+	var sign = event.shiftKey ? 1 : -1;
+
+	switch(e.keyCode){
+		
+		// R
+		case 82: /* R */	rgb = 0;					break;		
+		case 71: /* G */	rgb = 1;					break;		
+		case 66: /* B */	rgb = 2;					break;
+
+		case 49: /* 1 */	d1 += sign * 0.01;			break;	
+		case 50: /* 2 */	d2 += sign * 0.1;			break;	
+		case 51: /* 3 */	mode = 3;					break;	
+		case 52: /* 4 */	mode = 4;					break;	
+		case 53: /* 5 */	mode = 5;					break;	
+		case 54: /* 6 */	mode = 6;					break;	
+		case 55: /* 7 */	SetColor('rgb(205,85,0');	break;	
+		case 56: /* 8 */	SetColor('rgb(185,65,0');	break;	
+		case 57: /* 9 */	SetColor('rgb(255,0,255');	break;	
+		case 48: /* 0 */	SetColor('rgb(125,0,125');	break;	
+		default:
+			break;
+    }
+
+}
+
+
